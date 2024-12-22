c50d990d2620f1887f29ce09eecb93554195edcf
jasontankapps <jasontankapps@gmail.com>
2024-12-21 20:55:52 -0600

    NPC bugs *NPC pages weren't routing correctly to page 6 *some pages had bold formatting getting missed *other misc format edits

--- src/pages/NPCPage.tsx
+++ src/pages/NPCPage.tsx
@@ -10,16 +10,16 @@ const NPCGroup1Page = lazy(() => import("./NPCGroup1Page"));
 const NPCGroup2Page = lazy(() => import("./NPCGroup2Page"));
 const NPCGroup3Page = lazy(() => import("./NPCGroup3Page"));
 const NPCGroup4Page = lazy(() => import("./NPCGroup4Page"));
-const NPCGroup5Page = lazy(() => import("./NPCGroup4Page"));
-const NPCGroup6Page = lazy(() => import("./NPCGroup5Page"));
+const NPCGroup5Page = lazy(() => import("./NPCGroup5Page"));
+const NPCGroup6Page = lazy(() => import("./NPCGroup6Page"));
 
 const pages = [
 	({id}: {id: string}) => <NPCGroup1Page id={id} />,
 	({id}: {id: string}) => <NPCGroup2Page id={id} />,
 	({id}: {id: string}) => <NPCGroup3Page id={id} />,
 	({id}: {id: string}) => <NPCGroup4Page id={id} />,
-	({id}: {id: string}) => <NPCGroup4Page id={id} />,
 	({id}: {id: string}) => <NPCGroup5Page id={id} />,
+	({id}: {id: string}) => <NPCGroup6Page id={id} />,
 ]
 
 const NPCPage: React.FC = () => {
