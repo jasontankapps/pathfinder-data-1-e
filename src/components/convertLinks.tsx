9b74aeb6214a2aeabffe4549e8c7f7f3e0fd72da
jasontankapps <jasontankapps@gmail.com>
2025-03-07 03:51:22 -0600

    more editing and linking; {link} change *Can now use brackets inside links to omit characters from the link :: e.g. {x/p[s] and q/s} => [ps and qs](x/p_and_q) *modified a bunch of links to use that format *formatting and linking a lot of stuff in prestige classes, mostly

--- src/components/convertLinks.tsx
+++ src/components/convertLinks.tsx
@@ -13,28 +13,45 @@ export const checkForEncodedLink = (input: string, normal: boolean = false): fal
 	}
 	const [x, pre, protocol, matchedx, post] = m;
 	let matched = matchedx, linkpre = "", linkpost = "", textpre = "", textpost = "";
+	// pre_<link
 	if(m = matched.match(/(^.+?)<(.+$)/)) {
 		matched = m[2];
 		linkpre = m[1];
 	}
+	// post>_link
 	if(m = matched.match(/(^.+)>(.+$)/)) {
 		matched = m[1];
 		linkpost = m[2];
 	}
+	// Double-slashes
 	while(m = matched.match(/(^.+?)\/\/(.+$)/)) {
 		matched = `${m[1]}=slAsh=${m[2]}`;
 	}
+	// pre|text
 	if(m = matched.match(/(^.+?)\|(.+$)/)) {
 		matched = m[2];
 		textpre = m[1];
 	}
+	// post/text
 	if(m = matched.match(/(^.+?)\/(.+$)/)) {
 		matched = m[1];
 		textpost = m[2];
 	}
+	// enclosed [extra ]text
+	let temp = matched;
+	let linkmatched = "";
+	matched = "";
+	while(m = temp.match(/^(.*?)\[(.*?)\](.*)$/)) {
+		const [ignore, pre, extra, post] = m;
+		matched = matched + pre + extra;
+		linkmatched = linkmatched + pre;
+		temp = post;
+	}
+	matched = matched + temp;
+	linkmatched = linkmatched + temp;
 	const text = `${textpre}${matched}${textpost}`
 		.replace(/=slAsh=/g, "/");
-	const property = `${linkpre}${matched}${linkpost}`
+	const property = `${linkpre}${linkmatched}${linkpost}`
 		.replace(/[- ]|=slAsh=/g, "_")
 		.toLowerCase()
 		.replace(/[^a-z0-9_]/g, "");
@@ -47,7 +64,7 @@ export const checkForEncodedLink = (input: string, normal: boolean = false): fal
 // The rest of the text is converted to a link following these steps:
 //   All letters are converted to lowercase
 //   Spaces and dashes are turned into underscores
-//   All other non-letter, non-number, non-underscore ccharacters are deleted
+//   All other non-letter, non-number, non-underscore characters are deleted
 // Advanced forms:
 //   A second slash adds to the text, not the link:
 //     {rule/Link/ plus additional text} => [Link plus additional text](rule/link)
@@ -59,6 +76,8 @@ export const checkForEncodedLink = (input: string, normal: boolean = false): fal
 //     {rule/Link>_2} => [Link](rule/link_2)
 //   Using < prepends link-formatted text to the link:
 //     {rule/special_<Link} => [Link](rule/special_link)
+//   Using [brackets] removes text from a link, but preserves it in the text:
+//     {rule/rule[s] of cool} => [rules of cool](rule/rule_of_cool)
 // The advanced forms can be used together in the order <|/>
 //   {rule/special_<Code//|Link//Text/ Stuff>_info} => [Code/Link/Text Stuff](rule/special_link_text_info)
 
