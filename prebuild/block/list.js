80d7025a0776c576fe02e506e92b874bdbfa041e
jasontankapps <jasontankapps@gmail.com>
2025-11-24 04:37:59 -0600

    Add ability blocks pt 12
    *trying to make easier-to-read blocks for static abilities
    *started classes3: unchained monk, ninja, occultist
    *misc minor edits

--- prebuild/block/list.js
+++ prebuild/block/list.js
@@ -1,5 +1,6 @@
 import { convertTextToLink } from '../tests/checkForEncodedLink.js';
 
+// ::link[Header]{all="one~two~three..." link="protocol" and? hl? sep?="~" comma?=", "}
 export const makeListBlock = ({
 	text,
 	maybeClear,
@@ -24,18 +25,23 @@ export const makeListBlock = ({
 	// HEADER
 	//
 	const head = `${maybeClear}${inline ? "" : "<p>"}<strong${hl ? ` className="hl"` : ""}>${text}:</strong>`;
+	const tail = inline ? "" : "</p>";
 	//
 	// CONSTRUCT LINKS
 	//
 	const output = all.split(sep).map(item => 
 		`<Link to="/${link}/${convertTextToLink(item)}">${item}</Link>`
 	);
-	if(and && output.length >= 3) {
+	if(and) {
 		// Return list with 'and' if needed
-		const pop = output.pop();
-		return head + " " + output.join(comma) + comma + "and " + pop + (inline ? "" : "</p>");
+		if(output.length >= 3) {
+			const pop = output.pop();
+			return head + " " + output.join(comma) + comma + "and " + pop + tail;
+		} else if (output.length === 2) {
+			return `${head} ${output[0]}${and ? " and" : comma} ${output[1]}${tail}`;
+		}
 	}
-	return head + " " + output.join(comma) + (inline ? "" : "</p>");
+	return head + " " + output.join(comma) + tail;
 
 };
 
