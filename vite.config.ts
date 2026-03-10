274b00f5f2656fbad0e49ac25b76d151b760835e
jasontankapps <jasontankapps@gmail.com>
2026-03-10 03:14:50 -0500

    Add VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({babel: {configFile: true}}),
-    legacy()
+    legacy(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   test: {
     globals: true,
