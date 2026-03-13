245481e02854932082cb79986427f6dcb2e047ed
jasontankapps <jasontankapps@gmail.com>
2026-03-13 01:06:06 -0500

    Adjust Settings page

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonItem, IonLabel, IonList, IonPage, IonToggle } from '@ionic/react';
+import { IonContent, IonItem, IonLabel, IonList, IonPage, IonText, IonToggle } from '@ionic/react';
 import { useAppDispatch, useAppSelector } from './store/hooks';
 import { toggleNoElephant, toggleNoMythic, toggleNoStamina } from './store/settingsSlice';
 import PageHeader from './components/PageHeader';
@@ -12,26 +12,26 @@ const SettingsPage: React.FC = () => {
 
 	return (
 		<IonPage>
-			<PageHeader title="Settings" />
+			<PageHeader title="Settings" notBookmarkable />
 			<IonContent>
 				<IonList lines="full">
 					<IonItem lines="none">
 						<IonLabel><em>Some feats have extra options that can only be used if you are using the optional <Link to="/rule/stamina_and_combat_tricks">Stamina and Combat Tricks</Link> rules and have the <Link to="/feat/combat_stamina">Combat Stamina</Link> feat. If you're not using those rules or that feat, you can toggle this extra information off.</em></IonLabel>
 					</IonItem>
 					<IonItem className="mainItem basic">
-						<IonToggle justify="end" enableOnOffLabels checked={noStamina} onIonChange={() => dispatch(toggleNoStamina(!noStamina))} color="primary">Hide Combat Tricks</IonToggle>
+						<IonToggle justify="end" enableOnOffLabels checked={noStamina} onIonChange={() => dispatch(toggleNoStamina(!noStamina))} color="secondary"><IonText color="tertiary">Hide Combat Tricks</IonText></IonToggle>
 					</IonItem>
 					<IonItem lines="none">
 						<IonLabel><em>Some feats and spells have Mythic versions that can only be used if you're using the <Link to="/rule/mythic_rules">Mythic rules</Link>. If you're not using them, you can toggle these extra spells and feats off.</em></IonLabel>
 					</IonItem>
 					<IonItem className="mainItem basic">
-						<IonToggle justify="end" enableOnOffLabels checked={noMythic} onIonChange={() => dispatch(toggleNoMythic(!noMythic))} color="primary">Hide Mythic Spells/Feats</IonToggle>
+						<IonToggle justify="end" enableOnOffLabels checked={noMythic} onIonChange={() => dispatch(toggleNoMythic(!noMythic))} color="secondary"><IonText color="tertiary">Hide Mythic Spells/Feats</IonText></IonToggle>
 					</IonItem>
 					<IonItem lines="none">
 						<IonLabel><em><Link to="/rule/the_elephant_in_the_room">"The Elephant in the Room"</Link> is an optional, non-Paizo ruleset that changes the way feats work. Some feats and other pages will have extra information on them from this ruleset. If you're not using those rules, you can toggle this extra information off.</em></IonLabel>
 					</IonItem>
 					<IonItem className="mainItem basic">
-						<IonToggle justify="end" enableOnOffLabels checked={noElephant} onIonChange={() => dispatch(toggleNoElephant(!noElephant))} color="primary">Hide "Elephant in the Room" Rules</IonToggle>
+						<IonToggle justify="end" enableOnOffLabels checked={noElephant} onIonChange={() => dispatch(toggleNoElephant(!noElephant))} color="secondary"><IonText color="tertiary">Hide "Elephant in the Room" Rules</IonText></IonToggle>
 					</IonItem>
 				</IonList>
 			</IonContent>
