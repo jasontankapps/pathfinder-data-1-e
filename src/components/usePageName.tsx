355eda817a9e5d560e1e45afd3e02c7d3d22c475
jasontankapps <jasontankapps@gmail.com>
2026-05-03 02:57:35 -0500

    Bugfixes/comments in usePageName

--- src/components/usePageName.tsx
+++ src/components/usePageName.tsx
@@ -3,42 +3,68 @@ import { useAppSelector } from '../store/hooks';
 import { BookmarkDB } from '../store/bookmarksSlice';
 import index from '../json/_GEN_allLinks';
 
-const switchback = (link: string, db: BookmarkDB = {}): string => {
+interface Options1 {
+	db: BookmarkDB
+	booleanize?: never
+}
+interface Options2 {
+	db?: never
+	booleanize: boolean
+}
+type Options = Options1 | Options2
+
+function checkForPageName (id: string, opt: Options2): boolean
+function checkForPageName (id: string, opt: Options1): string
+function checkForPageName (id: string, opt: Options): string | boolean {
+	const { booleanize, db } = opt;
+	const link = id.slice(1);
 	// This matches only the non-fused pages
 	switch (link) {
 		case "":
-			return "Main";
+			return booleanize || "Main";
 		case "about":
-			return "About";
+			return booleanize || "About";
 		case "icons":
-			return "Icons";
+			return booleanize || "Icons";
 		case "settings":
-			return "Settings";
+			return booleanize || "Settings";
 		case "search":
-			return "Search";
+			return booleanize || "Search";
 		case "feattreespage":
-			return "Feat Trees";
+			return booleanize || "Feat Trees";
 		case "importexport":
-			return "Import/Export Bookmarks";
+			return booleanize || "Import/Export Bookmarks";
 		case "bookmarks":
-			return "Bookmarks";
+			return booleanize || "Bookmarks";
 	};
-	const m = link.match(/^[/]?bookmarks[/](.+)$/);
-	if(m) {
-		return "[B] " + ((db[m[1]] && db[m[1]].title) || "(Unknown Bookmark Group)");
+	// This matches fused pages.
+	if(index[link]) {
+		return booleanize || index[link];
+	} else if (booleanize) {
+		// We assume false at this point.
+		return false;
+	} else if (db) {
+		// Now check to see if we're a bookmark group
+		const m = link.match(/^bookmarks[/](.+)$/);
+		if(m) {
+			const group = db[m[1]];
+			return "[B] " + ((group && group.title) || "(Unknown Bookmark Group)");
+		}
 	}
+	// Return an error, otherwise
 	return `[Error: ${link} not found]`;
 }
 
 export const doesPageExist = (id: string): boolean => {
-	// Remove initial slash ("/main" => "main")
-	const link = id.slice(1);
-	return (switchback(link) || index[link]) ? true : false;
+	// NOTE: This will return false for bookmark groups
+	// This is acceptable, as this code is only used on
+	//   bookmarked pages, and groups cannot be bookmarked
+	return checkForPageName(id, { booleanize: true });
 };
 
 const usePageName = (): ((id: string) => string) => {
 	const db = useAppSelector(state => state.bookmarks.db);
-	const getPageName = useCallback((x: string) => switchback(x, db), [db]);
+	const getPageName = useCallback((x: string) => checkForPageName(x, { db }), [db]);
 	return getPageName;
 };
 
